c10bb33b0028410d6b7a6446f1b3f648c2dcacb8
hunt4991 <hunt4991@ISS2TTPKH2.scdsb.pri>
2018-11-09 14:24:17 -0500

    Assignment complete along with note!

---
+++ src/searching/ISSStudent.java
@@ -0,0 +1,31 @@
+package searching;
+public class ISSStudent implements Comparable{
+    private String name;
+    private String address;
+    private int id;
+    
+    ISSStudent(String n, String a, int i){
+        name = n;
+        address = a;
+        id = i;
+    }
+    
+    @Override
+    public String toString(){
+        return "Name:\t\t" + name + "\nAddress:\t" + address + 
+                "\nID:\t\t" + id;
+    }
+    
+    public int getID(){
+        return id;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(this.id > ((ISSStudent)o).getID())
+            return 1;
+        else if(this.id < ((ISSStudent)o).getID())
+            return -1;
+        else return 0;
+    }
+}
